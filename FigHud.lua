26340f8fbc37c8116def03fb47b1be0d689d0aad
Tony <tonyanziano5@gmail.com>
2023-01-20 18:12:35 -0800

    Added code to hide certain frames when hud frames are enabled.

--- FigHud.lua
+++ FigHud.lua
@@ -1,1 +1,16 @@
--- 
+local playerFrameEnabled = true
+local targetFrameEnabled = false
+local focusFrameEnabled = false
+
+-- hide default frames
+if playerFrameEnabled then
+  PlayerFrame:Hide()
+end
+
+if targetFrameEnabled then
+  
+end
+
+if focusFrameEnabled then
+  
+end
