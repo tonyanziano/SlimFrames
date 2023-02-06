c8af65daae3391d467ea9ced576a1a15c2a6ab0b
Tony <tonyanziano5@gmail.com>
2023-02-06 10:41:24 -0800

    Added some TODO notes on global frame initialization

--- SlimFrames.lua
+++ SlimFrames.lua
@@ -1,3 +1,7 @@
+-- TODO: these should probably moved to individual frame files / modules
+-- NOTE: or maybe we could have some frame initialization code here
+-- that loads the coming config DB and then creates a frame based on if the
+-- module is enabled or not
 local playerFrameEnabled = true
 local targetFrameEnabled = false
 local focusFrameEnabled = false
